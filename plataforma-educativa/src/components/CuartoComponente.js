280ce3b89419bc66460f7387586d7404e7acb3eb
Deomiro <deomiro.contreras23b@udgvirtual.udg.mx>
2024-05-05 10:40:44 -0600

    first commit

--- plataforma-educativa/src/components/CuartoComponente.js
+++ plataforma-educativa/src/components/CuartoComponente.js
@@ -4,8 +4,10 @@ export const CuartoComponente = () => {
   return (
     <div>
         
-        
-
+    <h1>TERCERA FASE DEL PROGRESO DE DESARROLLO DE SPRINT</h1>  
+ <p>Se avanzó como se había esperado. Los compañeros trabajaron en el desarrollo del Sprint 1 el cual ternimó favorablemente. </p>
+<p>Se hiciero ajustes en la organización se tiempos. Se dio más tiempo para que los desarrolladores terminaran la actividad</p>
+<p>El ScrumMaster pidió menos tiempo entre una reunión y otra para trabajar de forma más cercana con los desarrolladores.</p>
     </div>
   )
 }
---
+++ plataforma-educativa/src/components/SegundoComponente.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const SegundoComponente = () => {
+  return (
+    <div>
+        <h2>Calendario</h2>
+        <table>
+            <tr>
+                <td>LUNES</td><td>MARTES</td><td>MIÉRCOLES</td><td>JUEVES</td><td>VIERNES</td></tr>
+            <tr><td></td><td></td><td></td><td></td><td>Revisión de schedule para primer sprint del procucto backlog</td></tr>
+            <tr><td></td><td></td><td> Revisión detallada del proyecto, aspectos básicos y revisión del primer sprint y edición del backlog</td></tr>
+            <tr><td>Evaluación de los avances. Detección de problemas encontrados y solución de dudas </td></tr>
+
+            <tr><td></td><td></td><td></td><td></td><td>Reunión con Scrum Master para Evaluación de Sprint</td></tr>
+            <tr><td></td><td></td><td> REvisión y ajustes al Sprint 2 para el desarrollo de la Plataforma Educativa</td></tr>
+            <tr><td>Asignación de roles para Sprint 2</td></tr>
+        </table>
+        </div>
+  )
+}
